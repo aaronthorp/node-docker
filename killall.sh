71facb25daf87baa6e6edc6103619aaa5e9b1143
Aaron Thorp <hello@aaronthorp.com>
2015-08-27 22:15:43 +1000

    latest

--- killall.sh
+++ killall.sh
@@ -1,4 +1,2 @@
 #/bin/bash
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
 #/bin/bash
+docker run -d \
+  -p 8400:8400 \
+  -p 8500:8500 \
+  -p 8600:53/udp \
+  -h node1 \
+  --name "consul" \
+  progrium/consul -server -bootstrap
+
+docker run -d \
+  --name=registrator \
+  --net=host \
+  --volume=/var/run/docker.sock:/tmp/docker.sock \
+  gliderlabs/registrator:latest \
+  consul://localhost:8500
+
 docker run -d -p 81:80 --name app1 app
 docker run -d -p 82:80 --name app2 app
 docker run -d -p 83:80 --name app3 app
 docker run -d -p 84:80 --name app4 app
-docker run -d -p 80:80 --name haproxy haproxy
+
+docker run -d -p 80:80 --name haproxy --link consul haproxy
