48101c9e44c4cb3562a233870870fd48035b8bc8
Aaron Thorp <hello@aaronthorp.com>
2015-08-27 17:01:50 +1000

    ha

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-docker run -d -p 81:80 app
-docker run -d -p 82:80 app
-docker run -d -p 83:80 app
-docker run -d -p 84:80 app
-docker run -d -p 80:80 haproxy
+docker run -d -p 81:80 --name app1 app
+docker run -d -p 82:80 --name app2 app
+docker run -d -p 83:80 --name app3 app
+docker run -d -p 84:80 --name app4 app
+docker run -d -p 80:80 --name haproxy haproxy
--- run1.sh
+++ run1.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker run -d -p 81:80 app
+docker run -d -p 81:80 --name app1 app
